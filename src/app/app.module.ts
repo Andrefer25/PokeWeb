8cfb2eb241903b85100c089879863dddb3c937fb
André Malpica <andrefer2505@gmail.com>
2018-12-05 17:11:17 -0300

    ver 0.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoticiasComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
